423e0c0dc4c4776079fcbcb990cadb52871e5142
LeezyLazyCrazy <leeg1dance@gmail.com>
2022-03-10 15:37:29 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
+function onLoginSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  painGreetings(username);
+  // greeting.innerText ="Hello " + username; //아래는 또 다른 방법 
+  // greeting.innerText =`Hello ${username}`; // 꼭 백틱사용
+ 
+}
+
+function painGreetings(username){
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+  //show the greetings
+ painGreetings(savedUsername);
+}
+
+
+
+
+
+
